9ff6924d4d18ba16ae643f8c09849558d7f0c054
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-05-16 18:56:37 +0200

    Deleted compiled typescript files from source.

--- src/js/helpers/common.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.times = function (i, cb, l) {
-    if (l === void 0) { l = i; }
-    if (i === 0) {
-        return;
-    }
-    cb(l - i);
-    exports.times(i - 1, cb, l);
-};
