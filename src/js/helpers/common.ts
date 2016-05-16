1ac8364f97dc41c8689ada1bc52db3e46e95c9a5
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-05-16 18:51:31 +0200

    Added array shuffle function to common helpers.

--- src/js/helpers/common.ts
+++ src/js/helpers/common.ts
@@ -6,3 +6,20 @@ export let times = (i: number, cb: (n: number) => any, l = i) => {
   cb(l - i);
   times(i - 1, cb, l);
 };
+
+export let shuffle = (array: Array<any>): Array<any> => {
+  let currentIndex = array.length;
+  let temporaryValue: any;
+  let randomIndex: number;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
