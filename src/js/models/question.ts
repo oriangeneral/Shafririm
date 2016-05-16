2a80b8bdb73450ad907c25f839c8f365445ef0b9
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-05-16 14:03:13 +0200

    Models for questions.

--- src/js/models/question.ts
+++ src/js/models/question.ts
@@ -1,3 +1,23 @@
-export interface Question {
+export enum QuestionType {
+  AlbumNameFromImage,
+  TrackNameFromPreview,
+  ArtistNameFromTrackName,
+  ArtistNameFromAlbumName,
+  ReleaseDateFromTrackName,
+  ReleaseDateFromAlbumName,
+  TrackOnAlbumByName,
+  TrackPositionOnDisk
+}
 
+export interface Answer {
+  title: string;
+  correct: boolean;
+}
+
+export interface Question {
+  type: QuestionType;
+  title: string;
+  description: string;
+  answers: Answer[];
+  answered: boolean;
 }
