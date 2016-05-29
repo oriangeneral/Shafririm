7e79590d291906c7ecf990e8c1d27476b0ac4153
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-05-29 14:41:44 +0200

    Integrated materialize-css.

--- src/js/components/landing/landing.component.ts
+++ src/js/components/landing/landing.component.ts
@@ -2,14 +2,32 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import landingTemplate from './landing.html';
+import landingStyle from './landing.css';
+
+import { MaterializeDirective } from "angular2-materialize";
 
 @Component({
   selector: 'landing',
   template: landingTemplate,
+  styles: [landingStyle],
   directives: [
-    ROUTER_DIRECTIVES
+    ROUTER_DIRECTIVES,
+    MaterializeDirective
   ]
 })
 export class LandingComponent {
+
+  constructor() {
+    this.selectOptions = [
+      {
+        name: 'Austria',
+        value: 'at'
+      },
+      {
+        name: 'Germany',
+        value: 'de'
+      }
+    ]
+  }
 
 }
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/browser.d.ts" />
+// /// <reference path="../../typings/browser.d.ts" />
 
 import { provide } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
