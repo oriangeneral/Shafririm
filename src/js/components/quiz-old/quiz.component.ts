89287d5977c8f0b00e17c5111d0868aa7751e613
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-05-30 21:19:43 +0200

    Temporarily saved old quiz component.

---
+++ src/js/components/quiz-old/quiz.component.ts
@@ -0,0 +1,45 @@
+import { Component, Inject, AfterViewInit, ViewChild } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+import { PlaylistService } from '../../services/playlist.service';
+import { QuizService } from '../../services/quiz.service';
+import { QuestionsComponent } from '../questions/questions.component';
+import { QuestionComponent } from '../questions/question/question.component';
+
+import quizTemplate from './quiz.html';
+
+@Component({
+  selector: 'quiz',
+  template: quizTemplate,
+  directives: [
+    ROUTER_DIRECTIVES,
+    QuestionsComponent,
+    QuestionComponent
+  ],
+  providers: [
+    HTTP_PROVIDERS,
+    PlaylistService,
+    QuizService
+  ]
+})
+export class QuizComponent implements AfterViewInit {
+
+  @ViewChild(QuestionsComponent)
+  private _questionsComponent: QuestionsComponent;
+
+  constructor( @Inject(QuizService) private _quizService: QuizService) { }
+
+  public ngAfterViewInit() {
+    this.quizService.questionsComponent = this.questionsComponent;
+  }
+
+  get quizService(): QuizService {
+    return this._quizService;
+  }
+
+  get questionsComponent(): QuestionsComponent {
+    return this._questionsComponent;
+  }
+
+}
