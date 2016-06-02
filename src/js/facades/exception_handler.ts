af63cf3e49c423b6ecb2bdbad56719ee426265a1
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-06-02 18:44:49 +0200

    Show a generic error modal, when an exception occurs.

--- src/js/facades/exception_handler.ts
+++ src/js/facades/exception_handler.ts
@@ -1,4 +1,5 @@
 import { ExceptionHandler } from '@angular/core/src/facade/exception_handler';
+import $ from 'jquery';
 
 export interface ExceptionHandlerContract {
   call(exception: any, stackTrace?: any, reason?: string): void;
@@ -12,6 +13,13 @@ export class AppExceptionHandler implements ExceptionHandlerContract {
       // No need for reporting an error.
       return;
     }
+
+    $('#exception-modal').openModal({
+      dismissible: false,
+      opacity: .3,
+      in_duration: 200,
+      out_duration: 150
+    });
 
     console.groupCollapsed('Whoops, something went wrong.');
     console.warn('Please run the app in development mode to debug.');
