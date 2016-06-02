b44d8535456ffe30589f386356c6ca216991c699
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-06-02 20:48:18 +0200

    Preparations for deployment.

--- server/index.js
+++ server/index.js
@@ -23,13 +23,15 @@ if (process.argv.indexOf('dev') !== -1) {
 
 console.log('Server public dir set to ' + config.publicDir);
 
-try {
-  fs.accessSync(envFile, fs.F_OK)
-} catch(e) {
-  fs.writeFileSync(envFile, fs.readFileSync(envFile + '.example'));
-}
+if (!config.production) {
+  try {
+    fs.accessSync(envFile, fs.F_OK)
+  } catch (e) {
+    fs.writeFileSync(envFile, fs.readFileSync(envFile + '.example'));
+  }
 
-env(__dirname + '/.env');
+  env(__dirname + '/.env');
+}
 
 // Compress responses
 app.use(compress());
@@ -45,17 +47,21 @@ app.use('/api', require('./routes/api'));
 
 // Serve index if file does not exist
 app.get('*', function(req, res, next) {
-    if (!req.isFile) {
-        return res.sendFile('index.html', { root: path.join(__dirname, config.publicDir) });
-    }
+  if (!req.isFile) {
+    return res.sendFile('index.html', {
+      root: path.join(__dirname, config.publicDir)
+    });
+  }
 
-    return next();
+  return next();
 });
 
 // Response middleware
 app.use(methodOverride());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(errorHandler({
   dumpExceptions: !config.production,
   showStack: !config.production
@@ -63,17 +69,19 @@ app.use(errorHandler({
 
 // SSL certificate
 var options = {
-    key: fs.readFileSync('./server/ssl/server.key'),
-    cert: fs.readFileSync('./server/ssl/server.crt')
+  key: fs.readFileSync('./server/ssl/server.key'),
+  cert: fs.readFileSync('./server/ssl/server.crt')
 };
 
 // Create a HTTP2 server
 require('spdy').createServer(options, app).listen(config.production ? 443 : 5000);
 
 // Create HTTP server for redirecting
-require('http').createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
+require('http').createServer(function(req, res) {
+  res.writeHead(301, {
+    "Location": "https://" + req.headers['host'] + req.url
+  });
+  res.end();
 }).listen(config.production ? 80 : 5001);
 
 console.log('Server listening at https://localhost:5000');
