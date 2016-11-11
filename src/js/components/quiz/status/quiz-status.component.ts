c48472572720dc320bfd88369c48987be5b646c3
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-11-11 01:37:33 +0100

    Improved song playback in quiz status component.

--- src/js/components/quiz/status/quiz-status.component.ts
+++ src/js/components/quiz/status/quiz-status.component.ts
@@ -17,6 +17,8 @@ export class QuizStatusComponent {
 
   @Output() public modalActions = new EventEmitter<string | MaterializeAction>();
 
+  private _players = [];
+
   constructor(private _quizService: QuizService) {
 
   }
@@ -27,6 +29,61 @@ export class QuizStatusComponent {
 
   public closeModal() {
     this.modalActions.emit({ action: 'modal', params: ['close'] });
+  }
+
+  public stopAllPlayers() {
+    for (let p of this._players) {
+      this.stopPlayback(p.player);
+    }
+  }
+
+  public stopPlayback(player: HTMLAudioElement) {
+    let p = this.getPlayer(player);
+
+    p.isPlaying = false;
+    player.currentTime = 0;
+    player.pause();
+  }
+
+  public startPlayback(player: HTMLAudioElement) {
+    let p = this.getPlayer(player);
+    this.stopAllPlayers();
+
+    p.isPlaying = true;
+    player.currentTime = 0;
+    player.play();
+  }
+
+  public togglePlayback(player: HTMLAudioElement) {
+    if (player.ended || player.paused) {
+      this.startPlayback(player);
+      return;
+    }
+
+    this.stopPlayback(player);
+  }
+
+  public isAudioPlaying(player: HTMLAudioElement) {
+    let p = this.getPlayer(player);
+
+    if (p.isPlaying) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public getPlayer(player: HTMLAudioElement) {
+    for (let p of this._players) {
+      if (p.player === player) {
+        return p;
+      }
+    }
+
+    let entry = { player: player };
+    this._players.push(entry);
+
+    return entry;
   }
 
   get quizService() {
