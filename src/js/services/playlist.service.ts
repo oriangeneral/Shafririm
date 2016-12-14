8b1da4a8a5d6583f8f19c97726a77b0afd086202
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-12-14 09:54:56 +0100

    Removed rxjs cache operator.

--- src/js/services/playlist.service.ts
+++ src/js/services/playlist.service.ts
@@ -1,6 +1,5 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/cache';
 import 'rxjs/add/operator/catch';
 
 import { Injectable } from '@angular/core';
