cd13a6da67a8fa2ee8605a4c718de812f8322292
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-09-25 18:52:33 +0200

    Resolve missing hammerjs for materialize.

--- src/js/bootstrap.ts
+++ src/js/bootstrap.ts
@@ -1,5 +1,8 @@
+import * as Hammer from 'hammerjs';
+(window as any).Hammer = Hammer;
+
+import { AnimationBuilder } from 'css-animator/builder';
 import 'materialize-css';
 import 'angular2-materialize';
-import { AnimationBuilder } from 'css-animator/builder';
 
 AnimationBuilder.defaults.fixed = true;
