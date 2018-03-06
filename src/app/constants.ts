e6d1ec9263272fd21c2c8072fc7852ac340e547e
shlomiaz <shlomiaz@gmail.com>
2018-03-06 14:20:00 +0200

    added getscenarios and login functions to bl-service

---
+++ src/app/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+  public static readonly SCENARIO: string = 'scenario';
+  public static readonly DILEMA: string = 'dilema';
+  public static readonly USER: string = 'user';
+}
--- src/app/services/bl.service.ts
+++ src/app/services/bl.service.ts
@@ -1,6 +1,24 @@
 import {Injectable} from '@angular/core';
+import {Scenario} from '../models/scenario.model';
+import {User} from '../models/user.model';
+import {Dilema} from '../models/dilema.model';
+import {BlProxyService} from './bl-proxy.service';
+import {Constants} from '../constants';
 
 @Injectable()
 export class BlService {
+  public currentScenario: Scenario;
+  public currentDilema: Dilema;
+  public currentUser: User;
 
+  constructor(private blProxyService: BlProxyService) {
+  }
+
+  public getScenarios() {
+    return this.blProxyService.getAll(Constants.SCENARIO);
+  }
+
+  public login() {
+    return this.blProxyService.post(Constants.USER, this.currentUser);
+  }
 }
